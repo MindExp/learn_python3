9438c801d93a9d8fcf222d0bea48d5703c1a700b
MindExp <hezhihaics@gmail.com>
2018-07-03 14:39:38 +0800

    learn_python3.py

--- learn_python3.py
+++ learn_python3.py
@@ -1037,7 +1037,44 @@ h.hello()
 print(type(Hello), type(h), h, h.hello)
 
 ###############################
-# 回看使用元类
+# 参考资料：http://blog.jobbole.com/21351/
+# 元类用来创建类（对象）。即：元类为类的类
+"""
+MyClass = MetaClass()
+my_object = MyClass()
+
+MyClass = type(MyClass, (), {})
+type就是Python在背后用来创建所有类的元类
+"""
+# 元类会自动将你通常传给'type'的参数作为自己的参数传入
+# 注：以下程序在python3.6环境下测试失败
+def upper_attr(future_class_name, future_class_parents, future_class_attr):
+    """
+    返回一个类对象，将属性都转为大写形式
+    """
+    #  选择所有不以'__'开头的属性
+    attrs = ((name, value) for name, value in future_class_attr.items() if not name.startswith('__'))
+    # 将它们转为大写形式
+    uppercase_attr = dict((name.upper(), value) for name, value in attrs)
+    # 通过'type'来做类对象的创建
+    return type(future_class_name, future_class_parents, uppercase_attr)
+
+
+__metaclass__ = upper_attr  # 这会作用到这个模块中的所有类
+
+
+class Foo(object):
+    # 我们也可以只在这里定义__metaclass__，这样就只会作用于这个类中
+    bar = 'bip'
+
+
+print(hasattr(Foo, 'bar'))
+# 输出: False
+print(hasattr(Foo, 'BAR'))
+# 输出:True
+f = Foo()
+print(f.BAR)
+# 输出:'bip'
 
 ###############################
 import logging
